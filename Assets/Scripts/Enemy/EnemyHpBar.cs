a03bca0ab11d611b03989f7fc2fa9247f3d88305
ysh4267 <ysh4267@gmail.com>
2021-01-16 00:14:13 +0900

    적 체력바 구현
    기존 플레이어 체력바를 구현한것과 같은 방법으로 적의 체력바를 구현함.
    적의 체력바 슬라이더를 2개 생성하여 체력이 줄어들때 두개의 슬라이더가 시간차를 두고 줄어들도록 설정하여 데미지를 받은 효과를 구현함.

---
+++ Assets/Scripts/Enemy/EnemyHpBar.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyHpBar : MonoBehaviour {
+    public Slider hpSlider;
+    public Slider BackHpSlider;
+    public bool backHpHit = false;
+
+    public Transform enemy;
+    public float maxHp = 1000f;
+    public float currentHp = 1000f;
+
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        transform.position = enemy.position;
+        hpSlider.value = Mathf.Lerp(hpSlider.value, currentHp / maxHp, Time.deltaTime * 5f);
+
+        if (backHpHit) {
+            BackHpSlider.value = Mathf.Lerp(BackHpSlider.value, hpSlider.value, Time.deltaTime * 10f);
+            if (hpSlider.value >= BackHpSlider.value - 0.01f) {
+                backHpHit = false;
+                BackHpSlider.value = hpSlider.value;
+            }
+        }
+    }
+
+    public void Dmg() {
+        currentHp -= 900f;
+        Invoke("BackHpFun", 0.5f);
+    }
+    void BackHpFun() {
+        backHpHit = true;
+    }
+}
