21741f5a0c8dd1989854ef4795a55c5bd2e239f1
ysh4267 <ysh4267@gmail.com>
2021-03-20 12:58:46 +0900

    이전 작업내용 복기
    작업내용 주석처리

--- Assets/Scripts/Enemy/EnemyHpBar.cs
+++ Assets/Scripts/Enemy/EnemyHpBar.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class EnemyHpBar : MonoBehaviour {
+    //표시되는 HP바
     public Slider hpSlider;
+    //피격시에만 표시되는 HP바
     public Slider BackHpSlider;
+    //공격을 받았을때 뒤쪽 HP바가 줄어들도록 하기위한 불 값
     public bool backHpHit = false;
 
     public Transform enemy;
@@ -19,22 +22,24 @@ public class EnemyHpBar : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
-        transform.position = enemy.position;
-        hpSlider.value = Mathf.Lerp(hpSlider.value, currentHp / maxHp, Time.deltaTime * 5f);
-
+        transform.position = enemy.position; //HP바가 대상 위를 따라다니기위한 위치값
+        hpSlider.value = Mathf.Lerp(hpSlider.value, currentHp / maxHp, Time.deltaTime * 5f); //선형 보간함수를 이용해 체력이 서서히 줄어들도록 설계
         if (backHpHit) {
-            BackHpSlider.value = Mathf.Lerp(BackHpSlider.value, hpSlider.value, Time.deltaTime * 10f);
+            //맞아서 체력이 닳았는가
+            BackHpSlider.value = Mathf.Lerp(BackHpSlider.value, hpSlider.value, Time.deltaTime * 10f); //앞에 보이는 체력의 1/2의 속도로 선형 보간함수를 이용해 더 천천히 줄어들도록 설계
             if (hpSlider.value >= BackHpSlider.value - 0.01f) {
+                //뒤쪽 HP바가 앞쪽 HP바와 0.01이내로 가까워지면 함수 중단 이후 값을 같은수치로 변경
                 backHpHit = false;
                 BackHpSlider.value = hpSlider.value;
             }
         }
     }
-
+    //시험용 피격대미지 함수
     public void Dmg() {
         currentHp -= 900f;
         Invoke("BackHpFun", 0.5f);
     }
+
     void BackHpFun() {
         backHpHit = true;
     }
--- Assets/Scripts/Player/PlayerHpBar.cs
+++ Assets/Scripts/Player/PlayerHpBar.cs
@@ -24,13 +24,14 @@ public class PlayerHpBar : MonoBehaviour {
     }
 
     public void GetHpBoost() {
-        maxHp += 150;
-        float scaleX = (1000f / unitHp) / (maxHp / unitHp);
-        HpLineFolder.GetComponent<HorizontalLayoutGroup>().gameObject.SetActive(false);
+        maxHp += 150; //테스트용 체력증감식
+        float scaleX = (1000f / unitHp) / (maxHp / unitHp); //체력이 1000일때가 1:1 비율이고, 체력단위 한칸을 unitHp라 했을때 전체 체력에 보이는 칸의 수를 계산하는 식
+        HpLineFolder.GetComponent<HorizontalLayoutGroup>().gameObject.SetActive(false);//오브젝트가 켜져있을때 이미지의 크기조정이 적용되지않아 끈 상태로 사이즈를 변경
         foreach (Transform child in HpLineFolder.transform) {
-            child.gameObject.transform.localScale = new Vector3(scaleX, 1, 1);
+            //체력단위칸을 포함하고있는 부모오브젝트의 모든 child(체력칸들)
+            child.gameObject.transform.localScale = new Vector3(scaleX, 1, 1); //모든 체력칸의 스케일을 조정
         }
-        HpLineFolder.GetComponent<HorizontalLayoutGroup>().gameObject.SetActive(true);
+        HpLineFolder.GetComponent<HorizontalLayoutGroup>().gameObject.SetActive(true);//사이즈 변경후 오브젝트를 켬
 
     }
 }
